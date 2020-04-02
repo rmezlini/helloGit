07e7f3beb9721eb1cba54b646afa72f6d737df75
Rached Mezlini <rachedmezlini@yahoo.com>
2020-04-02 12:23:12 +0100

    premier commit

--- helloGit/src/git/demo/HelloGit.java
+++ helloGit/src/git/demo/HelloGit.java
@@ -5,7 +5,6 @@ public class HelloGit {
 	public static void main(String [] args) {
 		System.out.println("demo git");
 		System.out.println("nouvelle branche");
-		System.out.println("nouvelle branche");
 	}
 
 }
