61659f07c7ae9c5f2013eb2de9ae4ed386dbbc9f
Rached Mezlini <rachedmezlini14@gmail.com>
2020-04-16 19:13:57 +0100

    cc

--- src/ccc/vvv/vvv/HelloGit.java
+++ src/ccc/vvv/vvv/HelloGit.java
@@ -1,11 +1,12 @@
-package git.demo;
+package ccc.vvv.vvv;
 
-public class RenameHelloGit {
+public class HelloGit {
 	
 	public static void main(String [] args) {
 		System.out.println("demo git");
 		System.out.println("demo git");
 		System.out.println("nouvelle branche");
+		System.out.println("conflict");
 		System.out.println("conflict");
 	}
 
